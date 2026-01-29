37ae2433817d64034228f528f5cf2f02baa07e1c
rachel-bryant <rachel.bryant@du.edu>
2026-01-28 22:33:22 -0700

    audio

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// TODO: let user name dog
 // TODO: add audio
+// TODO: disable all buttons when dog is sleeping
 
 // ACCESS HTML ELEMENTS
 const dog_container = document.getElementById("dog-container");
+const dog = document.querySelector(".dog");
+
+// sound effect variables
+const bark = document.getElementById("bark");
+const pant = document.getElementById("pant");
+const eat = document.getElementById("eat");
 
 // upgrade variables
 const points_dis = document.getElementById("pets");
@@ -17,12 +23,14 @@ const sad_icon = document.getElementById("sad");
 const hungry_icon = document.getElementById("hungry");
 const sick_icon = document.getElementById("sick");
 const dirty_icon = document.getElementById("dirty");
+const tired_icon = document.getElementById("tired");
 
 // stat displays
 const happy_dis = document.getElementById("happy-stat");
 const hunger_dis = document.getElementById("hunger-stat");
 const health_dis = document.getElementById("health-stat");
 const clean_dis = document.getElementById("clean-stat");
+const energy_dis = document.getElementById("energy-stat");
 
 // get stat colors from CSS variables
 const rootStyles = getComputedStyle(document.documentElement);
@@ -31,12 +39,14 @@ const sad = rootStyles.getPropertyValue('--sad').trim();
 const hungry = rootStyles.getPropertyValue('--hungry').trim();
 const sick = rootStyles.getPropertyValue('--sick').trim();
 const dirty = rootStyles.getPropertyValue('--clean').trim();
+const tired = rootStyles.getPropertyValue('--energy').trim();
 
 // access buttons 
 const play_btn = document.getElementById("play-btn");
 const feed_btn = document.getElementById("feed-btn");
 const sick_btn = document.getElementById("medicine-btn");
 const clean_btn = document.getElementById("clean-btn");
+const sleep_btn = document.getElementById("sleep-btn");
 const upgrade_btn = document.getElementById("upgrade-btn");
 
 // initialize stats
@@ -44,6 +54,7 @@ let happy_stat = 100;
 let hunger_stat = 0;
 let health_stat = 100;
 let clean_stat = 100;
+let energy_stat = 100;
 
 let colors_by_occurance = []; // used to track which order background colors come, used for healing stats
 
@@ -103,8 +114,26 @@ const clean_interval = setInterval(function(){
     }
 }, 5000);
 
+// energy stat will decrease every 2.5 seconds
+const energy_interval = setInterval(function(){
+    if (energy_stat > 0){
+        energy_stat--;
+        energy_dis.innerHTML = "Energy: " + energy_stat;
+    }
+    if (energy_stat == 49) {
+        colors_by_occurance.push(tired);
+        dog_container.style.background = tired;
+        no_warnings.style.display = "none";
+        tired_icon.style.display = "block";
+    }
+}, 2500);
+
 // get 1 point every time you pet the dog
-document.querySelector(".dog").onclick = function() {
+dog.onclick = function() {
+    const bark_play = Math.floor(Math.random()*101);
+    if (bark_play > 80) { // only bark sometimes
+        bark.play();
+    }
     pets++;
     points_dis.innerText = pets;
 }
@@ -133,6 +162,7 @@ function disable_btn(btn, time) {
 
 // play fetch
 play_btn.onclick = function() {
+    pant.play();
     happy_stat += boost;
     if (happy_stat > 100) { // max happy stat is 100
         happy_stat = 100;
@@ -140,7 +170,9 @@ play_btn.onclick = function() {
     happy_dis.innerHTML = "Happiness: " + happy_stat;
 
     if (happy_stat >= 50) {
-        colors_by_occurance.splice(colors_by_occurance.indexOf(sad), 1);
+        if (sad.includes(colors_by_occurance)) {
+            colors_by_occurance.splice(colors_by_occurance.indexOf(sad), 1);
+        }
         if (colors_by_occurance.length == 0) {
             dog_container.style.background = happy;
         } else {
@@ -154,6 +186,12 @@ play_btn.onclick = function() {
 
 // feed dog
 feed_btn.onclick = function() {
+    eat.play();
+    setTimeout(function(){
+        eat.pause();
+        eat.currentTime = 0;
+    }, 5000);
+
     hunger_stat -= boost;
     if (hunger_stat < 0) { // max happy stat is 100
         hunger_stat = 0;
@@ -161,7 +199,9 @@ feed_btn.onclick = function() {
     hunger_dis.innerHTML = "Hunger: " + hunger_stat;
 
     if (hunger_stat <= 51) {
-        colors_by_occurance.splice(colors_by_occurance.indexOf(hungry), 1);
+        if (hungry.includes(colors_by_occurance)) {
+            colors_by_occurance.splice(colors_by_occurance.indexOf(hungry), 1);
+        }
         if (colors_by_occurance.length == 0) {
             dog_container.style.background = happy;
         } else {
@@ -175,6 +215,7 @@ feed_btn.onclick = function() {
 
 // give dog medicine
 sick_btn.onclick = function() {
+    bark.play();
     health_stat += boost;
     if (health_stat > 100) { // max happy stat is 100
         health_stat = 100;
@@ -182,7 +223,9 @@ sick_btn.onclick = function() {
     health_dis.innerHTML = "Health: " + health_stat;
 
     if (health_stat >= 50) {
-        colors_by_occurance.splice(colors_by_occurance.indexOf(sick), 1);
+        if (sick.includes(colors_by_occurance)) {
+            colors_by_occurance.splice(colors_by_occurance.indexOf(sick), 1);
+        }
         if (colors_by_occurance.length == 0) {
             dog_container.style.background = happy;
         } else {
@@ -203,7 +246,9 @@ clean_btn.onclick = function() {
     clean_dis.innerHTML = "Cleanliness: " + clean_stat;
 
     if (clean_stat >= 50) {
-        colors_by_occurance.splice(colors_by_occurance.indexOf(dirty), 1);
+        if (dirty.includes(colors_by_occurance)) {
+            colors_by_occurance.splice(colors_by_occurance.indexOf(dirty), 1);
+        }
         if (colors_by_occurance.length == 0) {
             dog_container.style.background = happy;
         } else {
@@ -213,6 +258,32 @@ clean_btn.onclick = function() {
         check_icons();
     }
     disable_btn(clean_btn, 25000);
+}
+
+// put dog to sleep
+sleep_btn.onclick = function() {
+    dog.src = "images/sleep.png";
+    document.body.style.background = "#8888FF";
+    disable_btn(sleep_btn, 30000);
+
+    setTimeout(function(){ // dog wakes up after 10s
+        dog.src = "images/dog.png";
+        document.body.style.background = "#eff7ff";
+        energy_stat = 100;
+        energy_dis.innerHTML = "Energy: " + clean_stat;
+        if (energy_stat >= 50) {
+            if (tired.includes(colors_by_occurance)) {
+                colors_by_occurance.splice(colors_by_occurance.indexOf(tired), 1);
+            }
+            if (colors_by_occurance.length == 0) {
+                dog_container.style.background = happy;
+            } else {
+                dog_container.style.background = colors_by_occurance[colors_by_occurance.length-1];
+            }
+            tired_icon.style.display = "none";
+            check_icons();
+        }
+    }, 10000);
 }
 
 // upgrades boost
